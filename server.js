194c995471562d53b46437dddf063c5c2f3dca07
Phillip Quy Le <pitlv2109@gmail.com>
2016-07-21 14:25:38 -0500

    Update Heroku port

--- server.js
+++ server.js
@@ -65,8 +65,10 @@ app.use(function(request, response) {
 })
 
 // Use Heroku port
-app.listen(process.env.PORT || <default port>)
+app.set('port', (process.env.PORT || 5000));
 
-console.log("Server is running at 8080");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
