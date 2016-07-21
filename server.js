19df1fc6f2f3d06129ef30c9dbb2393776a3db4c
Phillip Quy Le <pitlv2109@gmail.com>
2016-07-21 03:51:25 +0000

    Create public/index.html and staticly serve it on server.js

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
-var express = require("express");
-var app = express();
+// Import Express framework
+var express = require("express");	
+// Create an Express instance
+var app = express();	
 
-app.use(function(request, response) {
+// Handle request
+app.use(function(request, response, next) {
 	if (request.url === "/") { 
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.end("Hello World");
+		next();
+		console.log("Got a request");
 	}	
+	
+	//TODO:
+	// Handle route for the following formats
+	// https://timestamp-ms.herokuapp.com/December%2015,%202015
+	// https://timestamp-ms.herokuapp.com/December 15 2015
+	// https://timestamp-ms.herokuapp.com/1450137600
+	//
+	// If route is correct, display in JSON format as
+	// { "unix": 1450137600, "natural": "December 15, 2015" }
+	//
+	// Else { "unix": null, "natural": null}
+	
 	else {
 		response.writeHead(404, {"Content-Type": "text/plain"});
 		response.end("404, Page Not Found!");
 	}
-}).listen(8080);
+})
+
+app.use(express.static("./public"));
+
+app.listen(8080);
 
 console.log("Server is running at 8080");
 
