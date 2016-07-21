e3ca1ae6469d1aac61824b0d128976c85cd499d5
Phillip Quy Le <pitlv2109@gmail.com>
2016-07-21 16:31:10 +0000

    Handle .../unix, .../invalid, .../

--- server.js
+++ server.js
@@ -1,33 +1,67 @@
 // Import Express framework
-var express = require("express");	
+var express = require("express");
+
+// Import file system
+var fs = require("fs");
+
 // Create an Express instance
 var app = express();	
 
+var nullJSON = {
+	unix: null,
+	natural: null
+}
+
+var dateJSON = {
+	unix: "",
+	natural: ""
+}
+
 // Handle request
-app.use(function(request, response, next) {
+app.use(function(request, response) {
+	
+	// Handle homepage request
+	// Show usage and example by public/index.html
 	if (request.url === "/") { 
-		next();
-		console.log("Got a request");
+		var stream = fs.createReadStream("./public/index.html");
+		stream.pipe(response);
+		console.log("Got a request to the home page");
+		
 	}	
 	
-	//TODO:
-	// Handle route for the following formats
-	// https://timestamp-ms.herokuapp.com/December%2015,%202015
-	// https://timestamp-ms.herokuapp.com/December 15 2015
-	// https://timestamp-ms.herokuapp.com/1450137600
-	//
-	// If route is correct, display in JSON format as
-	// { "unix": 1450137600, "natural": "December 15, 2015" }
-	//
-	// Else { "unix": null, "natural": null}
-	
 	else {
-		response.writeHead(404, {"Content-Type": "text/plain"});
-		response.end("404, Page Not Found!");
+		
+		var route = request.url.substring(1, request.url.length);
+		var date;
+		
+		// If route is a number
+		if (!isNaN(route)) {
+			date = new Date(parseInt(route));
+			console.log(date);
+		}
+		
+		// If route is a string
+		else {
+			date = new Date(route);
+		}
+		
+		// Handle date request
+		// Respond with a JSON format date
+		if (date.toString() !== "Invalid Date") {
+			dateJSON.unix = parseInt(route);
+			dateJSON.natural = date.toString().substring(4, date.toString().length - 24);
+			response.json(dateJSON);
+			console.log("Got a request to show a date");
+		}
+		
+		// Invalid route
+		// Respond with a null JSON format date
+		else {
+			response.json(nullJSON);
+			console.log("Invalid route");
+		}
 	}
 })
-
-app.use(express.static("./public"));
 
 app.listen(8080);
 
